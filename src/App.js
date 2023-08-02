aac8357cd41aef8ac421b62a9f4c0a71776da8b1
Mikadjx <109203536+Mikadjx@users.noreply.github.com>
2023-08-02 21:41:29 +0200

    Mise en place du Header et Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import Footer from "./components/footer";
+import Header from "./components/header";
+import Content from "./components/content";
+import styles from"./App.module.scss";
 
 function App() {
-  return <h1>Cookchef</h1>;
+  return (
+<div className={`d-flex flex-column ${styles.appContainer}`}>
+  <Header />
+ <Content />
+<Footer />
+  </div>
+  );
 }
 
 export default App;
---
+++ src/components/content.js
@@ -0,0 +1,4 @@
+function Content() {
+    return <div className="flex-fill"></div>;
+}
+export default Content;
