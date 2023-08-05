c5b169b97d63bd47a0f2258f5325114d364a5978
Mikadjx <109203536+Mikadjx@users.noreply.github.com>
2023-08-05 23:28:48 +0200

    Responsive avec mixins

--- src/components/content.js
+++ src/components/content.js
@@ -10,7 +10,7 @@ function Content() {
     
     <div className="flex-fill container p-20">
 <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
-<div className={`card p-20 ${styles.contentCard}`}>
+<div className={`card p-20 ${styles.contentCard} br`}>
 <div className={styles.grid}>
 
 {recipes.map((r) => (
