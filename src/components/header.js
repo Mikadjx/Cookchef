e93a39f3367fa28c6a974c46502427aa6fc0592f
Mikadjx <109203536+Mikadjx@users.noreply.github.com>
2023-08-07 12:40:27 +0200

    création du menu burger

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,35 @@
 import styles from "../header.module.scss";
-import cookchef from "../assets/images//Red Vintage Shrimp Seafood Logo.png";
+import cookchef from "../assets/images/Orange and Brown Culinary Kitchen Logo (1).png";
+import { useState} from "react";
+import HeaderMenu from "./HeaderMenu";
 
 function Header() {
+   const [showMenu, setShowMenu] = useState(false);
     return (
     <header className={`${styles.header} d-flex align-items-center`}>
 
-    <i class="fa-solid fa-bars mr-15"></i>
+  
     <div className="flex-fill">
         <img src={cookchef} alt="logo cookchef" />
         </div>
     
-    <ul>
+    <ul className={styles.headerList}>
         <button className="mr-5 btn btn-reverse-primary">
-        <i class="fa-solid fa-basket-shopping mr-5"></i>
+        <i class="fa-solid fa-heart mr-5"></i>
             
-            <span>panier</span> </button>
+            <span>Wishlist</span> </button>
         <button className="btn btn-primary">connexion</button>
     
     </ul>
     
+    <i onClick={() => setShowMenu(true)} className={`fa-solid fa-bars ${styles.headerXs}`}></i>
+ {showMenu && (
+    <>
+    <div onClick={() => setShowMenu(false)} className="calc"></div>
+    <HeaderMenu />
+    </>
+ )}
+ 
     </header>
 
     );
